57912d0d2f1d63047647f73455dc069ba38031fb
OskarCreate <oskar_morales_f@outlook.com>
2025-05-02 01:48:24 -0500

    Arreglando errores de migración

--- Data/Migrations/20250502063651_NewMigracion.Designer.cs
+++ Data/Migrations/20250502063651_NewMigracion.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Adopcion_mascotas.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250501224126_NewMigracion")]
+    [Migration("20250502063651_NewMigracion")]
     partial class NewMigracion
     {
         /// <inheritdoc />
@@ -75,7 +75,6 @@ namespace Adopcion_mascotas.Data.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<string>("ImageURL")
-                        .IsRequired()
                         .HasColumnType("TEXT");
 
                     b.Property<string>("NombreMascota")
